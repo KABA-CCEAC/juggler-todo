db693c03812202555f68a5fb18ecc792e7d087b1
Jan Muehlemann <jan.muehlemann@gmail.com>
2013-02-05 09:24:13 +0100

    create the server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+  , app = express();
+
+// load json file
+var data = require('./data');
+
+// server list of all todos
+app.get('/todos', function(req, res) {
+
+  // some 'fancy' no-cache headers
+  res.header('Cache-Control', 'no-cache, must-revalidate');
+  res.header('Pragma', 'no-cache');
+  res.header('Expires', -1);
+
+  // just return the data object as json
+  res.json(data);
+});
+
+
+// start server
+app.listen(3000);
+console.log('Listening on port 3000');
